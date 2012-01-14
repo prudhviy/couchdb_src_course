014d56e70b85cfa48cbd8a17c477e3d40a90fa6d
prudhviy <prudhviy@gmail.com>
2012-01-14 20:30:01 +0530

    added more funs to q3 and coding standards to q1

--- week1/q3.erl
+++ week1/q3.erl
@@ -3,8 +3,10 @@
 -export([perimeter/1]).
 
 perimeter({square, Side}) ->
-	4*Side;
+	4 * Side;
 perimeter({circle, Radius}) ->
 	2 * (22/7) * Radius;
 perimeter({triangle, A, B, C}) ->
-	A + B + C.
+	A + B + C;
+perimeter({rectangle, Length, Width}) ->
+	2 * Length + 2 * Width.
