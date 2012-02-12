50388d4fd38e1765246c5e13be13652269bff3a6
prudhviy <prudhviy@gmail.com>
2012-02-13 02:59:23 +0530

    added compressor functions to kitchen cooler

--- week3/kitchen.erl
+++ week3/kitchen.erl
@@ -4,24 +4,28 @@
 
 fridge1(FoodList) ->
 	receive
-		{From, {store, Food}} ->
+		{From, {store, {Food, Temp}}} ->
 			From ! {self(), ok},
-			fridge1([Food|FoodList]);
+			fridge1([{Food, Temp}|FoodList]);
 		{From, {take, Food}} ->
 			case lists:member(Food, FoodList) of
 				true ->
 					From ! {self(), {ok, Food}},
 					fridge1(lists:delete(Food, FoodList));
 				false ->
-					From ! {self(), not_found},
+					from ! {self(), not_found},
 					fridge1(FoodList)
 			end;
+		{From, {cool, By_temp, Min_temp}} ->
+			Cooled_items = [ {Item, Temp - By_temp} || {Item, Temp}<- FoodList, Temp =< Min_temp],
+			From ! {self(), {cooled, Cooled_items}},
+			fridge1(Cooled_items);
 		terminate ->
-			ok
+			{ok, stopped}
 	end.
 
-store(Food) ->
-	fridge ! {self(), {store, Food}},
+store(Food, Temp) ->
+	fridge ! {self(), {store, {Food, Temp}}},
 	receive
 		{Pid, Msg} -> Msg
 	after 3000 ->
@@ -36,16 +40,38 @@ take(Food) ->
 		timeout
 	end.
 
-start() ->
-	restart().
+%% our desired temperature is 30 degrees and for every 3 secs
+%% reduce temp by 2 degrees
+cooler(Time) ->
+	fridge ! {self(), {cool, 2, 30}},
+	receive
+		{Pid, {cooled, FoodList}} ->
+			io:format("current temp and items: ~n~p~n", [FoodList]),
+			cooler(Time);
+		terminate ->
+			{ok, stopped}
+	after Time ->
+		fridge ! {self(), {cool, 2}},
+		cooler(Time)	
+	end.
 
-restart() ->
+start_compressor() ->
+	Pid = spawn_link(?MODULE, cooler, [2000]),
+	{ok, Pid}.
+	
+start() ->
+	spawn(?MODULE, restart, [[{apple, 35},{banana, 40}]]).
+
+restart(FoodList) ->
 	process_flag(trap_exit, true),
-	Pid = spawn_link(?MODULE, fridge1, [apple, banana]),
+	Pid = spawn_link(?MODULE, fridge1, [[]]),
 	register(fridge, Pid),
+	start_compressor(),
 	receive
 		{'EXIT', Pid, normal} -> {ok, normal_exit};
 		{'EXIT', Pid, shutdown} -> {ok, shutdown_exit};
-		{'EXIT', Pid, _} -> restart()
+		{'EXIT', Pid, _} -> {unknown, Pid}
 	end.
 
+stop() ->
+	fridge ! terminate.
